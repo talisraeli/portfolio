5b8c9f0c093a7ae6018a5242964d0ec893f53083
KeshetBehanan <contact@talisrae.li>
2023-05-01 03:20:30 +0300

    Cleared the project and added Sass

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import { render } from "preact";
+import { Route, Router } from "preact-router";
+import "./style.sass";
+
+const Main = () => <></>;
+
+render(<Main />, document.getElementById("app"));
