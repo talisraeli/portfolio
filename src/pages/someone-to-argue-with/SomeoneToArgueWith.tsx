4dafe5c5e1f4ba5f1b91b5da9221c4deaba49351
KeshetBehanan <contact@talisrae.li>
2023-05-04 04:43:51 +0300

    Added custom page title

--- src/pages/someone-to-argue-with/SomeoneToArgueWith.tsx
+++ src/pages/someone-to-argue-with/SomeoneToArgueWith.tsx
@@ -1,7 +1,14 @@
+import { useEffect } from "preact/hooks";
 import style from "./SomeoneToArgueWith.module.sass";
 import Hero from "./hero/Hero";
 
 export default function SomeoneToArgueWith() {
+  useEffect(() => {
+    const prev = document.title;
+    document.title = `Someone to Argue With | ${prev}`;
+    return () => (document.title = prev);
+  }, []);
+
   return (
     <>
       <Hero />
