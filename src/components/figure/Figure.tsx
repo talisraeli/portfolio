5be518d7cc63ec3e4982aee5f276704baa56081f
KeshetBehanan <contact@talisrae.li>
2023-05-04 14:46:41 +0300

    Added rounded corners to figure image

--- src/components/figure/Figure.tsx
+++ src/components/figure/Figure.tsx
@@ -4,12 +4,17 @@ import style from "./Figure.module.sass";
 interface FigureProps {
   caption?: string;
   children?: JSX.Element;
+  noRoundedCorners?: boolean;
 }
 
 export default function Figure(props: FigureProps) {
   return (
-    <div className="wrapper-l">
-      <figure className={style.figure}>
+    <div className="wrapper-m">
+      <figure
+        className={`${style.figure}${
+          props.noRoundedCorners ? style.noRoundedCorners : ""
+        }`}
+      >
         {props.children}
         {props.caption && <figcaption>{props.caption}</figcaption>}
       </figure>
